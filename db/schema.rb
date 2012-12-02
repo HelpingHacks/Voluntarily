32c818f7ddad031861b65eec7cd8ea7cd6ce3fea
evx001 <eviexeris@yahoo.com>
2012-12-01 22:24:40 -0500

    migrated voluntr

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121202031258) do
+ActiveRecord::Schema.define(:version => 20121202032158) do
 
   create_table "miscs", :force => true do |t|
     t.string   "name"
@@ -182,6 +182,17 @@ ActiveRecord::Schema.define(:version => 20121202031258) do
     t.string   "misc"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
+  end
+
+  create_table "volunteers", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "password"
+    t.integer  "phone_number"
+    t.string   "location"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
 end
